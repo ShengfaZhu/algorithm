48588ed1fe857feccad2360aeae279c321fee97e
ShengfaZhu <1140973730@qq.com>
2018-06-10 21:30:42 +0800

    rewrite merge sort

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,73 +1,58 @@
+// merge sort recursive way
 #include <iostream>
-#include <cstdlib>//rand()
-#include "time.h"//time()
-#include <algorithm> sort
-
+#include <cstdlib> //rand()
+#include <climits>
+#include <cassert>
+#include <vector>
+#include <ctime>
 using namespace std;
 
-void merge(double *_Array, int p, int q, int r)//p:第0个，r:第n-1个，q:第(r+p)/2个
+void merge(vector<int>& nums, int p, int q, int r)
 {
+	assert(p <= q);
+	assert(q + 1 <= r);
 	int len1 = q - p + 1;
 	int len2 = r - q;
-	double *L = new double[len1 + 1];
-	double *R = new double[len2 + 1];
-	for (int i = 0; i < len1; i++)//Array数组左边放入L数组中
-	{
-		L[i] = _Array[p + i];
-	}
-	for (int i = 0; i < len2; i++)//Array数值右边放入R数值中
-	{
-		R[i] = _Array[q + 1 + i];
-	}
-	L[len1] = INT_MAX;
-	R[len2] = INT_MAX;//无穷大
-	int i = 0;
-	int j = 0;
-	for (int k = p; k <= r; k++)//将小的数值放入_Array数组中
-	{
-		if (L[i] < R[j])
-		{
-			_Array[k] = L[i];
+	vector<int> L(nums.begin() + p, nums.begin() + q + 1);
+	vector<int> R(nums.begin() + q + 1, nums.begin() + r + 1);
+	L.push_back(INT_MAX), R.push_back(INT_MAX);
+	int i = 0, j = 0, k = p;
+	while (k <= r) {
+		if (L[i] < R[j]) {
+			nums[k] = L[i];
 			i++;
 		}
-		else
-		{
-			_Array[k] = R[j];
+		else {
+			nums[k] = R[j];
 			j++;
 		}
+		k++;
 	}
 }
 
-void mergeSort(double _Array[], int p, int r)
+void merge_sort(vector<int>& nums, int low, int high)
 {
-	if (p < r)
+	if (low < high)
 	{
-		int q;
-		q= (p + r) / 2;
-		mergeSort(_Array, p, q);
-		mergeSort(_Array, q + 1, r);
-		merge(_Array, p, q, r);
+		int mid = (low + high) / 2;
+		merge_sort(nums, low, mid);
+		merge_sort(nums, mid + 1, high);
+		merge(nums, low, mid, high);
 	}
 }
 
 
-int main()
-{
-	int n = 100;
-	double *Array = new double[n];
-	srand((unsigned)time(0));
-	for (int i = 0; i < n; i++)
-	{
-		Array[i] = rand() % 101;//生成0-100之间的随机整数
-		cout << Array[i] << "\t";
-	}
-	//mergeSort(Array, 0, n-1);
-	sort(Array, Array + n);
-	cout << "\n" << "After sort : " << endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout << Array[i] << "\t";
-	}
-	return 0;
+int main() {
+    vector<int> nums;
+    for (int i = 0; i < 20; i++) {
+        nums.push_back(rand() % 20);
+    }
+    cout << "before sorting : " << endl;
+    for (int i : nums) cout << i << "\t";
+    merge_sort(nums, 0, nums.size() - 1);
+    cout << "\nafter sorting : \n";
+    for (int i : nums) cout << i << "\t";   
+    system("pause");
+    return 0;
 }
 
